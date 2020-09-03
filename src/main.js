bf652df7cbe8e1b3b4fc690c578a7f6a84dd464e
Seyyed Mehdi Hassanpour <seyyedkhandon@gmail.com>
2020-09-03 11:23:29 +0430

    phase1: templating is complete

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import fs from 'fs';
 import ncp from 'ncp';
 import path from 'path';
 import { promisify } from 'util';
+import execa from 'execa';
+import Listr from 'listr';
+import { projectInstall } from 'pkg-install';
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
@@ -13,15 +16,24 @@ async function copyTemplateFiles(options) {
     });
 }
 
+async function initGit(options) {
+    const result = await execa('git', ['init'], {
+        cwd: options.targetDirectory,
+    });
+    if (result.failed) {
+        return Promise.reject(new Error('Failed to initialize git'));
+    }
+    return;
+}
+
 export async function createProject(options) {
     options = {
         ...options,
-        targetDirectory: options.targetDirectory || process.cwd(),
+        targetDirectory: options.targetDirectory || process.cwd()
     };
 
-    const currentFileUrl = import.meta.url;
     const templateDir = path.resolve(
-        new URL(currentFileUrl).pathname.substring(new URL(currentFileUrl).pathname.indexOf('/') + 1),
+        new URL(import.meta.url).pathname.substring(new URL(import.meta.url).pathname.indexOf('/') + 1),
         '../../templates',
         options.template.toLowerCase()
     );
@@ -34,9 +46,30 @@ export async function createProject(options) {
         process.exit(1);
     }
 
-    console.log('Copy project files');
-    await copyTemplateFiles(options);
+    const tasks = new Listr([
+        {
+            title: 'Copy project files',
+            task: () => copyTemplateFiles(options),
+        },
+        {
+            title: 'Initialize git',
+            task: () => initGit(options),
+            enabled: () => options.git,
+        },
+        {
+            title: 'Install dependencies',
+            task: () =>
+                projectInstall({
+                    cwd: options.targetDirectory,
+                }),
+            skip: () =>
+                !options.runInstall
+                    ? 'Pass --install to automatically install dependencies'
+                    : undefined,
+        },
+    ]);
 
+    await tasks.run();
     console.log('%s Project ready', chalk.green.bold('DONE'));
     return true;
 }
