f1551c5751288f7ab4d679a09875e85c45281e56
Seyyed Mehdi Hassanpour <seyyedkhandon@gmail.com>
2020-09-03 10:26:41 +0430

    add simple cli using inquerior to ask about js or ts

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,65 @@
-export function cli(args) {
-    console.log(args);
+import arg from 'arg';
+import inquirer from 'inquirer';
+
+function parseArgumentsIntoOptions(rawArgs) {
+    const spec = {
+        '--git': Boolean,
+        '--yes': Boolean,
+        '--install': Boolean,
+        '-g': '--git',
+        '-y': '--yes',
+        '-i': '--install',
+    }
+    const option = {
+        argv: rawArgs.slice(2),
+    };
+    const args = arg(spec, option);
+    console.log(JSON.stringify(args));
+    return {
+        skipPrompts: args['--yes'] || false,
+        git: args['--git'] || false,
+        template: args._[0],
+        runInstall: args['--install'] || false,
+    };
+}
+async function promptForMissingOptions(options) {
+    const defaultTemplate = 'JavaScript';
+    if (options.skipPrompts) {
+        return {
+            ...options,
+            template: options.template || defaultTemplate,
+        };
+    }
+
+    const questions = [];
+    if (!options.template) {
+        questions.push({
+            type: 'list',
+            name: 'template',
+            message: 'Please choose which project template to use',
+            choices: ['JavaScript', 'TypeScript'],
+            default: defaultTemplate,
+        });
+    }
+
+    if (!options.git) {
+        questions.push({
+            type: 'confirm',
+            name: 'git',
+            message: 'Initialize a git repository?',
+            default: false,
+        });
+    }
+
+    const answers = await inquirer.prompt(questions);
+    return {
+        ...options,
+        template: options.template || answers.template,
+        git: options.git || answers.git,
+    };
+}
+export async function cli(args) {
+    let options = parseArgumentsIntoOptions(args);
+    options = await promptForMissingOptions(options);
+    console.log(options);
 }
